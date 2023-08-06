ac290b84a0417dc77c937057f8d7421dd53ae9ee
InnaKara <innatvinna@gmail.com>
2023-08-06 21:48:13 +0300

    HW2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const container = document.querySelector(".gallery");
+console.log(SimpleLightbox);
+
+function createMarkup(array) {
+  return array
+    .map(
+      ({ description, original, preview }) =>
+        `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+    )
+    .join("");
+}
+
+createMarkup(galleryItems);
+container.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
